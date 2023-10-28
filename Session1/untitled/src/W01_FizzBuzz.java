e5f625510e04aaeb5cc909b2830d75b9831660f7
Vlad Hudea-Nojogan <vhudeanojogan@gopro.com>
2023-10-28 11:13:48 +0300

    - minor bug fix

--- Session1/untitled/src/W01_FizzBuzz.java
+++ Session1/untitled/src/W01_FizzBuzz.java
@@ -1,6 +1,6 @@
 public class W01_FizzBuzz {
     public static void showFizzBuzz(int n) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= n; i++) {
             if ((i % 3 == 0) && (i % 5 == 0)) {
                 System.out.printf("FizzBuzz ");
                 continue;
