d75cdd2b08e17029e0d1293f06e17c11a8e539a0
Vlad Hudea-Nojogan <vhudeanojogan@gopro.com>
2023-10-28 12:30:05 +0300

    Update W03_Arrays.java

--- Session1/untitled/src/W03_Arrays.java
+++ Session1/untitled/src/W03_Arrays.java
@@ -10,11 +10,27 @@ public class W03_Arrays {
         return false;
     }
 
+    public static int numberOfGoodPairs(int[] numbers) {
+        int count = 0;
+        for (int i=0; i<numbers.length-1; i++) {
+            for (int j=i+1; j<numbers.length; j++) {
+                if (numbers[i] < numbers[j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
-        int[] array = {1, 3, 4, 4, 5};
+        int[] array = {1, 3, 6, 4, 5};
         int[] anotherArray = {1, 2, 3, 4, 1};
 
         System.out.println(containsDuplicate(array));
         System.out.println(containsDuplicate(anotherArray));
+
+        System.out.println(numberOfGoodPairs(array));
+        System.out.println(numberOfGoodPairs(anotherArray));
     }
 }
