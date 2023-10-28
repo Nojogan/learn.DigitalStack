baa285e15f53b7f282c8fadc130ed12bf23aeedf
Vlad Hudea-Nojogan <vhudeanojogan@gopro.com>
2023-10-28 11:11:01 +0300

    FizzBuzz

---
+++ Session1/untitled/src/W01_FizzBuzz.java
@@ -0,0 +1,25 @@
+public class W01_FizzBuzz {
+    public static void showFizzBuzz(int n) {
+        for (int i = 1; i <= 100; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.printf("FizzBuzz ");
+                continue;
+            }
+
+            if (i % 3 == 0) {
+                System.out.printf("Fizz ");
+                continue;
+            }
+
+            if (i % 5 == 0) {
+                System.out.printf("Buzz ");
+                continue;
+            }
+
+            System.out.printf(i + " ");
+        }
+    }
+    public static void main(String[] args) {
+        showFizzBuzz(100);
+    }
+}
